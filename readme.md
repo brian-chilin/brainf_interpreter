1a0a15dad92fe051873882c39f2104d2f2aca633
brian <lbc.brian.chilin@gmail.com>
2024-09-04 00:36:48 -0700

    start

---
+++ readme.md
@@ -0,0 +1,15 @@
+My implementation is missing , part of interpreter
+
+### CECS 524 Unit 1 Assignment  
+#### Programming - Due Sep 11, 2024, 5pm to Canvas.  
+
+Write, in the programming language of your choice, an interpreter for the language Brainfu*k. This is a Turing complete language. It is very simple, on the order of a late first semester of programming assignment. Did mine in Java, about 60 lines of code. Run it with this input below - what is the output?
+
+```
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+```
+
+The Wikipedia page has enough information on the functions of the commands to complete the assignment.
+Turn in your source code and a screen shoot of the output and the IDE behind it (or surrounding the output).
+
+Mac users! Please do not submit zipped directories of your program. And do not use Apple based file formats.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::env;
+use std::collections::VecDeque;
+
+
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<char> = env::args().collect::<Vec<String>>()[1].chars().collect();
+    println!("{:?}", input);
+
+    let mut cells: VecDeque<u64> = VecDeque::new();
+    cells.push_back(0);
+    let mut memory_pointer: usize = 0;
+    let mut instruction_pointer: usize = 0;
+
+    while instruction_pointer < input.len() {
+        let i = input[instruction_pointer as usize];
+        //println!("interpretting instruction {}", i);
+        
+        match i {
+            '>' => {
+                memory_pointer += 1;
+                if memory_pointer == cells.len() {
+                    cells.push_back(0);
+                }
+                instruction_pointer += 1;
+            },
+            '<' => {
+                if memory_pointer == 0 {
+                    cells.push_front(0);
+                } else {
+                    memory_pointer -= 1;
+                }
+                instruction_pointer += 1;
+            },
+            '+' => {
+                cells[memory_pointer] += 1;
+                instruction_pointer += 1;
+            },
+            '-' => {
+                cells[memory_pointer] -= 1;
+                instruction_pointer += 1;
+            },
+            '.' => {
+                instruction_pointer += 1;
+                println!("{}  todo .", cells[memory_pointer] as u8);
+            },
+            '[' => {
+                instruction_pointer += 1;
+                println!("  todo [");
+            },
+            ']' => {
+                instruction_pointer += 1;
+                println!("  todo ]");
+            },
+            _ => instruction_pointer += 1
+        }
+    }
+    println!("{:?}", (&cells,));
 }
