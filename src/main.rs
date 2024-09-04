1d849d396f9d7fa62bc99ec42c37c625069c2e8f
brian <lbc.brian.chilin@gmail.com>
2024-09-04 01:13:18 -0700

    seemingly working enough

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ use std::collections::VecDeque;
 
 fn main() {
     let input: Vec<char> = env::args().collect::<Vec<String>>()[1].chars().collect();
-    println!("{:?}", input);
+    //println!("{:?}", input);
 
-    let mut cells: VecDeque<u64> = VecDeque::new();
+    let mut cells: VecDeque<u8> = VecDeque::new();
     cells.push_back(0);
     let mut memory_pointer: usize = 0;
     let mut instruction_pointer: usize = 0;
 
     while instruction_pointer < input.len() {
-        let i = input[instruction_pointer as usize];
-        //println!("interpretting instruction {}", i);
-        
+        let i = input[instruction_pointer];
+        //println!("interpreting instruction {}", i);
         match i {
             '>' => {
                 memory_pointer += 1;
@@ -41,18 +40,47 @@ fn main() {
             },
             '.' => {
                 instruction_pointer += 1;
-                println!("{}  todo .", cells[memory_pointer] as u8);
+                print!("{}", cells[memory_pointer] as char);
             },
             '[' => {
-                instruction_pointer += 1;
-                println!("  todo [");
+                if cells[memory_pointer] == 0 {
+                    while input[instruction_pointer] != ']' {
+                        instruction_pointer += 1
+                    }
+                    instruction_pointer += 1
+                } else {
+                    let mut bcount: usize = 1;
+                    while bcount > 0 {
+                        instruction_pointer += 1;
+                        match input[instruction_pointer] {
+                            '[' => bcount += 1,
+                            ']' => bcount -= 1,
+                            _k => () // do nothing
+                        }
+                    }
+                }
             },
             ']' => {
-                instruction_pointer += 1;
-                println!("  todo ]");
+                if cells[memory_pointer] != 0 {
+                    let mut bcount: usize = 1;
+                    while bcount > 0 {
+                        instruction_pointer -= 1;
+                        match input[instruction_pointer] {
+                            '[' => bcount -= 1,
+                            ']' => bcount += 1,
+                            _k => () // do nothing
+                        }
+                    }
+                    instruction_pointer += 1;
+                } else {
+                    instruction_pointer += 1;
+                }
             },
-            _ => instruction_pointer += 1
+            _c => {
+                println!("Continuing passed unknown instruction {}", _c);
+                instruction_pointer += 1
+            }
         }
     }
-    println!("{:?}", (&cells,));
+    //println!("{:?}", (&cells));
 }
